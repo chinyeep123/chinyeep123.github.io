7455b94550d1613faeac23b7e0fab169a31d4145
Git Patrick <patrick.wan@kinchotech.com>
2023-04-21 20:38:58 +0800

    fix: unknown images link

--- src/assets/projects/data.js
+++ src/assets/projects/data.js
@@ -3,42 +3,42 @@ export const projectsData = [
     projectName: "Zen2pos",
     projectDescription:
       "This is a SaaS POS System. It build from Laravel and Vue JS",
-    imageUrl: "http://comecodetogether.com/user-images/zen2pos.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/zen2pos.png",
     projectUrl: "http://zen2pos.com/",
   },
   {
     projectName: "Fission Pay",
     projectDescription:
       "This is a landing page, content is for crytocurrency. It build from HTML & JS",
-    imageUrl: "http://comecodetogether.com/user-images/fissionpay.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/fissionpay.png",
     projectUrl: "https://fissionpay.com/",
   },
   {
     projectName: "Kincho Tech Landing Page",
     projectDescription:
       "This is company landing page which is build from me. It build from HTML & JS",
-    imageUrl: "http://comecodetogether.com/user-images/kinchotech.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/kinchotech.png",
     projectUrl: "https://kinchotech.com/",
   },
   {
     projectName: "Kincho Tech WebDev Landing Page",
     projectDescription:
       "This is company web dev service landing page which is build from me. It build from HTML & JS",
-    imageUrl: "http://comecodetogether.com/user-images/kinchotech-webdev.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/kinchotech-webdev.png",
     projectUrl: "https://webdev.kinchotech.com/",
   },
   {
     projectName: "Kincho Tech AppDev Landing Page",
     projectDescription:
       "This is company app dev service landing page which is build from me. It build from HTML & JS",
-    imageUrl: "http://comecodetogether.com/user-images/kinchotech-appdev.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/kinchotech-appdev.png",
     projectUrl: "https://appdev.kinchotech.com/",
   },
   {
     projectName: "Kincho Tech Hosting Landing Page",
     projectDescription:
       "This is company hosting service landing page which is build from me. It build from HTML & JS",
-    imageUrl: "http://comecodetogether.com/user-images/kinchotech-hosting.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/kinchotech-hosting.png",
     projectUrl: "https://hosting.kinchotech.com/",
   },
   {
@@ -46,127 +46,128 @@ export const projectsData = [
     projectDescription:
       "This is company digital marketing service landing page which is build from me. It build from HTML & JS",
     imageUrl:
-      "http://comecodetogether.com/user-images/kinchotech-marketing.png",
+      "https://images.comecodetogether.com/imgs/kinchotech-marketing.png",
     projectUrl: "https://digitalmarketing.kinchotech.com/",
   },
   {
     projectName: "Billing Web App",
     projectDescription:
       "This is a billing web system that allow residences to make payment base on utilities, view statements, view statement detail & view payment history. It build from Laravel and Vue JS",
-    imageUrl: "http://comecodetogether.com/user-images/billing-web-app.png",
-    projectUrl: "http://web-app.comecodetogether.com/",
+    imageUrl: "https://images.comecodetogether.com/imgs/billing-web-app.png",
+    projectUrl: "https://web-app.comecodetogether.com/",
   },
   {
     projectName: "Star Property Virtual Fair",
     projectDescription:
       "This is a Fair which allow customer to visit via virtual. It do have exihibition via virtual, webinar registration, luckydraw & spin, survey form, keyword data search for property & location. It also have some articles related properties.",
-    imageUrl: "http://comecodetogether.com/user-images/smg.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/smg.png",
     projectUrl: "https://staging.2vr360.online/smuphd2021/",
   },
   {
     projectName: "Statement Analyzer Module",
     projectDescription:
       "This is a statement analyzer module library that suite to growcrm and also normal laravel, it allow you to do customize in it. It build from Laravel and Vue JS",
-    imageUrl: "http://comecodetogether.com/user-images/statement-module.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/statement-module.png",
     projectUrl: "https://github.com/chinyeep123/StatementAnalyzer",
   },
   {
     projectName: "Blogger Site",
     projectDescription:
       "This is a blogger site which is build from Wordpress. It's to share my programming experinces",
-    imageUrl: "http://comecodetogether.com/user-images/own-blog.png",
-    projectUrl: "http://comecodetogether.com/",
+    imageUrl: "https://images.comecodetogether.com/imgs/own-blog.png",
+    projectUrl: "https://czapp.comecodetogether.com/",
   },
   {
     projectName: "Odesi Tech",
     projectDescription: "This is a landing page which I build for my company",
-    imageUrl: "http://comecodetogether.com/user-images/odesi-tech.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/odesi-tech.png",
     projectUrl: "https://odesi.tech/",
   },
   {
     projectName: "Odesi Green Tech",
     projectDescription: "This is a landing page which I build for my company",
-    imageUrl: "http://comecodetogether.com/user-images/odesi-green.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/odesi-green.png",
     projectUrl: "https://green.odesi.tech/",
   },
   {
     projectName: "Odesi Count",
     projectDescription: "This is a landing page which I build for my company",
-    imageUrl: "http://comecodetogether.com/user-images/odesi-count.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/odesi-count.png",
     projectUrl: "https://odesicount.com/",
   },
   {
     projectName: "Odesi Life",
     projectDescription: "This is a landing page which I build for my company",
-    imageUrl: "http://comecodetogether.com/user-images/odesi-life.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/odesi-life.png",
     projectUrl: "https://odesi.life/",
   },
   {
     projectName: "eCOB MY",
     projectDescription: "This is a landing page which I build for my company",
-    imageUrl: "http://comecodetogether.com/user-images/ecob-my.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/ecob-my.png",
     projectUrl: "https://ecob.my/",
   },
   {
     projectName: "Vibrant Stream",
     projectDescription: "This is a landing page which I build for my company",
-    imageUrl: "http://comecodetogether.com/user-images/vibrant-stream.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/vibrant-stream.png",
     projectUrl: "https://vibrantstreams.tech/",
   },
   {
     projectName: "Carlsberg CNY 2019",
     projectDescription:
       "It is an unity apps upload images, gallery site. It allow user to search their code that given by the apps, so user can share to facebook or email to itself.",
-    imageUrl: "http://comecodetogether.com/user-images/carlsberg-cny.jpg",
+    imageUrl: "https://images.comecodetogether.com/imgs/carlsberg-cny.jpg",
     projectUrl: "https://carlsbergcny2019.my/",
   },
   {
     projectName: "Carlsberg Golf Classic Win Wheel Lucky Draw",
     projectDescription:
       "This is a registration form with a luckydraw win wheel. The admin will create an event for every luckydraw. Admin can control the winner base on the percentage.",
-    imageUrl: "http://comecodetogether.com/user-images/carlsberg-win-wheel.jpg",
+    imageUrl:
+      "https://images.comecodetogether.com/imgs/carlsberg-win-wheel.jpg",
     projectUrl: "https://carlsberggolfclassic.com.my/home/9xmt6",
   },
   {
     projectName: "Carlsberg E-Shop",
     projectDescription:
       "This is a E-Commerce Website with reporting. All of the flow have been done. It left payment gateway.",
-    imageUrl: "http://comecodetogether.com/user-images/carlsberg-eshop.jpg",
+    imageUrl: "https://images.comecodetogether.com/imgs/carlsberg-eshop.jpg",
     projectUrl: "https://shop.probablythebestbrewerytour.com.my/",
   },
   {
     projectName: "Carlsberg Top Ten Charity",
     projectDescription: "This is a registration form with reporting site.",
     imageUrl:
-      "http://comecodetogether.com/user-images/carlsberg-top-ten-charity.jpg",
+      "https://images.comecodetogether.com/imgs/carlsberg-top-ten-charity.jpg",
     projectUrl: "https://tten.my/",
   },
   {
     projectName: "BMW Road Show Site",
     projectDescription:
       "It is an unity apps upload images, gallery site. It allow user to search their code that given by the apps, so user can share to facebook or email to itself. Example code : BMW3SERIES00246, BMW3SERIES00245",
-    imageUrl: "http://comecodetogether.com/user-images/bmw-road-show.jpg",
+    imageUrl: "https://images.comecodetogether.com/imgs/bmw-road-show.jpg",
     projectUrl: "https://vfie.my/",
   },
   {
     projectName: "Packist.com",
     projectDescription:
       "It is a travel platform for user to book travel package and flight.",
-    imageUrl: "http://comecodetogether.com/user-images/packist.jpg",
+    imageUrl: "https://images.comecodetogether.com/imgs/packist.jpg",
     projectUrl: "http://packist.com/",
   },
   {
     projectName: "GetPackn",
     projectDescription:
       "It is a travel platform for user to book travel package and flight. (only for Australia package)",
-    imageUrl: "http://comecodetogether.com/user-images/getpackn.jpg",
+    imageUrl: "https://images.comecodetogether.com/imgs/getpackn.jpg",
     projectUrl: "http://getpackn.com/",
   },
   {
     projectName: "Sureplify",
     projectDescription:
       "It is a insurance website, there provide a detail of the insurance and some insurance package.",
-    imageUrl: "http://comecodetogether.com/user-images/sureplify.jpg",
+    imageUrl: "https://images.comecodetogether.com/imgs/sureplify.jpg",
     projectUrl: "http://sureplify.com/",
   },
   {
@@ -180,26 +181,26 @@ export const projectsData = [
   {
     projectName: "API Integration",
     projectDescription: "This is a simple node js and express js api",
-    imageUrl: "http://comecodetogether.com/user-images/api.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/api.png",
     projectUrl: "https://github.com/chinyeep123/node_js_rest_api",
   },
   {
     projectName: "Livewire Wizard Form",
     projectDescription:
       "This is an example wizard form build in Laravel Livewire",
-    imageUrl: "http://comecodetogether.com/user-images/example-wizard.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/example-wizard.png",
     projectUrl: "http://cct.comecodetogether.com/example/wizard",
   },
   {
     projectName: "Livewire Upload Photo Form",
     projectDescription: "This is a upload form build in Laravel Livewire",
-    imageUrl: "http://comecodetogether.com/user-images/example-upload.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/example-upload.png",
     projectUrl: "http://cct.comecodetogether.com/example/upload-photo",
   },
   {
     projectName: "Signature Pad",
     projectDescription: "This is a signature pad example with js",
-    imageUrl: "http://comecodetogether.com/user-images/example-signature.png",
+    imageUrl: "https://images.comecodetogether.com/imgs/example-signature.png",
     projectUrl: "https://cct.comecodetogether.com/example/signature-pad",
   },
 ];
